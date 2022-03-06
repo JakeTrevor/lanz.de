133f5778bc371682da016554c9287f4439efd7ae
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2022-03-06 23:31:58 +0000

    fixed typing issue in getters

--- src/scripts/getters.ts
+++ src/scripts/getters.ts
@@ -1,25 +1,27 @@
-const CosmosClient = require("@azure/cosmos").CosmosClient;
-const config = require("./cosmosConfig");
-const { endpoint, key, databaseId, containerId } = config;
+import { CosmosClient } from "@azure/cosmos";
+import config from "./cosmosConfig";
+
+let { endpoint, key, databaseId, containerId } = config;
 
 const client = new CosmosClient({ endpoint, key });
 const database = client.database(databaseId);
 const container = database.container(containerId);
 
 //returns the db response to creating a new item
-function upload(newItem){
-        return container.items.create(newItem);
-} 
+function upload(newItem: { name: string }) {
+  return container.items.create(newItem);
+}
+
 //takes an sqlQuery as a string returns a list
-function query(sqlQuery){
-        const querySpec = {query:sqlQuery}
-        return container.items.query(querySpec).fetchAll()
+//?of what?
+async function query(sqlQuery: string) {
+  const querySpec = { query: sqlQuery };
+  return await container.items.query(querySpec).fetchAll();
 }
 
 //give this the item you wish to delete (like as a jason)
-function deleteItem(item){
-        return container.item.deleteItem(item)
+function deleteItem(item: string) {
+  return container.item(item).delete;
 }
 
-export {upload,query,deleteItem}
-
+export { upload, query, deleteItem };
