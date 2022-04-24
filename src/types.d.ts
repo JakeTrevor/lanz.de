794455d8b9df15a84df4c3872e741354e4722d00
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2022-04-24 15:22:52 +0100

    created types defining DB schema

--- src/types.d.ts
+++ src/types.d.ts
@@ -4,9 +4,48 @@ interface albumLink {
   tooltip: string;
 }
 
-interface albumMetadata {
-  name: string;
+interface albumDisplayData {
   description: string;
   location: string;
   date: string;
+}
+
+interface photoDisplayData {
+  href: string;
+  caption: string;
+}
+
+//* data schema for cosmos DB
+
+// for data about albums themselves
+interface albumData {
+  name: string;
+  description: string;
+  min_description: string;
+  location: string;
+  date: string;
+  thumbnail: string; // a reference to a photo in the album witch should be displayed as the link
+  type: "albumData";
+}
+
+// data about photos in an album
+interface photoData {
+  href: string;
+  caption: string;
+  album: string;
+  type: "photoData";
+}
+
+// data concerning slots on the index page
+interface slotPhoto {
+  href: string;
+  slot_id: string;
+  type: "slotPhoto";
+}
+
+// alternate schema:
+interface slotData {
+  photos: string[];
+  slot_id: string;
+  type: "slotData";
 }
