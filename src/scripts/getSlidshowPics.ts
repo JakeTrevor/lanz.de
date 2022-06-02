1f35ae00d846819cf0cae1fc441c3c3027e747f6
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2022-06-02 13:16:17 +0100

    pushing all work for move

--- src/scripts/getSlidshowPics.ts
+++ src/scripts/getSlidshowPics.ts
@@ -2,9 +2,24 @@
 // return a source url
 
 import * as fs from "fs";
+import { query } from "./getters";
+
+export async function getSlot(id: any): Promise<string[]> {
+  let q = ` 
+  SELECT *
+  FROM pictures
+  WHERE pictures.type = "slotPhoto" AND pictures.slot_id= "${id}"
+  `;
+
+  let result = await query(q);
+  let slides = result.map((each) => each.href);
+  return slides;
+}
 
 // pictures at the very top
 export function getSlot1(): string[] {
+  //alt:
+  // return getSlot(1)
   let location = String(import.meta.env.PUBLIC_FRONT_PAGE_SLIDES);
 
   let slides = fs.readdirSync(location);
